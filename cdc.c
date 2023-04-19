0c4da0e982616554b6ca8e5016357a70b7ed0dac
collapsedonion <82096865+collapsedonion@users.noreply.github.com>
2023-04-19 20:30:49 +0900

    добавлены функции работы с динамическими библиотеками

--- cdc.c
+++ cdc.c
@@ -1,8 +1,10 @@
 #include "cdc.h"
 #include <stdlib.h>
+#include <string.h>
 #ifdef __APPLE__
 	#include <unistd.h>
 	#include <mach-o/dyld.h>
+	#include <dlfcn.h>
 #endif
 
 
@@ -31,4 +33,37 @@ char* cdc_get_executable_directory(){
 	return path;
 #endif
 }
+
+//opens dynamic library, reqires path to library without file extension (will be added automaticly)
+cdc_dynamic_lib_handle cdc_open_dynamic_lib(char* path_to_dl){
+	size_t new_str_len = strlen(path_to_dl);
+#ifdef __APPLE__
+	new_str_len += 6;
+	char* extension = ".dylib";
+#endif
+	char* real_path = (char*)malloc(new_str_len + 1);
+	real_path = memcpy(real_path, path_to_dl, strlen(path_to_dl));
+	real_path = strcat(real_path, extension);
+
+#ifdef __APPLE__
+	void* handler = dlopen(real_path, RTLD_NOW);	
+#endif	
+	free(real_path);
+	return (cdc_dynamic_lib_handle)(handler);
+}
+
+//extracts pointer to symbol at dynamic library
+void* cdc_get_dynamic_lib_member(cdc_dynamic_lib_handle dl_handle, char* member_name){
+#ifdef __APPLE__
+	return dlsym(dl_handle, member_name);
+#endif
+}
+
+//close dynamic library
+void cdc_close_dynamic_lib(cdc_dynamic_lib_handle dl_handle){
+#ifdef __APPLE__
+	dlclose(dl_handle);
+#endif
+}
+
 
--- cdc.h
+++ cdc.h
@@ -1,9 +1,32 @@
 #ifndef _CDC
 #define _CDC
 
+#ifdef __APPLE__
+#ifdef __cplusplus
+	#define DL_EXTERN extern "C"
+#else
+	#define DL_EXTERN extern
+#endif
+#endif
+
+typedef void* cdc_dynamic_lib_handle; 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 //returns current work directory
 char* cdc_get_current_directory();
 //returns path to the current running executable
 char* cdc_get_executable_directory();
+//opens dynamic library, reqires path to library without file extension (will be added automaticly)
+cdc_dynamic_lib_handle cdc_open_dynamic_lib(char* path_to_dl);
+//extracts pointer to symbol at dynamic library
+void* cdc_get_dynamic_lib_member(cdc_dynamic_lib_handle dl_handle, char* member_name);
+//close dynamic library
+void cdc_close_dynamic_lib(cdc_dynamic_lib_handle dl_handle);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
